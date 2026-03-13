6b75e55397563ed7440e8c6d0f3d960f7ca00989
Tejas <gvssrk776@gmail.com>
2026-03-13 15:21:03 +0530

    Initial commit: Added Student Dashboard UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
+import StudDashboard from './Pages/stud_Dashboard';
+
 export default function App() {
   return (
-    <div className="h-screen flex items-center justify-center bg-blue-900">
-      <h1 className="text-4xl font-bold text-white">Credit Snap is Ready! 🚀</h1>
-    </div>
+    <StudDashboard />
   )
 }
