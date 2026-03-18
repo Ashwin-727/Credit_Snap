2d0a70f025ec083e2dd8dfdf1489f6df4fc6593e
Ashwin Panchaneni <panchaneniashwinrao@gmail.com>
2026-03-18 21:56:57 +0530

    backend of menu management

--- Backend/app.js
+++ Backend/app.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const cors = require('cors');
+const canteenRoutes = require('./routes/canteenRoutes'); // Add near the top
+
+// ... your other app.use code ...
+
 
 const app = express();
 
 // 1. Middlewares
 app.use(cors()); // Allows your React frontend to talk to this backend
 app.use(express.json()); // Allows your server to understand JSON data
-
+app.use('/api/canteens', canteenRoutes); // Hook it up!
 // 2. A simple test route!
 app.get('/', (req, res) => {
   res.send('Hello from the CreditSnap Backend Engine!');
