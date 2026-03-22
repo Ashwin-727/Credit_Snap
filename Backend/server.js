d2f992c3d934903cf07dadca2f38327819721084
SaiShreyas1 <saishreyaskasavajjala@gmail.com>
2026-03-23 01:24:30 +0530

    Small changes

--- Backend/server.js
+++ Backend/server.js
@@ -11,7 +11,7 @@ const httpServer = createServer(app);
 
 const io = new Server(httpServer, {
   cors: {
-    origin: 'http://localhost:5173',
+    origin: 'http://localhost:5173,http://localhost:5174',
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
   },
 });
